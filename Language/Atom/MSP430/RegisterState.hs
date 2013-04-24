88a6bd8ab04892bd49f078de62c57bd03df7880b
Daniel Buckmaster <dan.buckmaster@gmail.com>
2013-04-24 15:12:04 +1000

    Updated the way RegisterState works.
    Now its state includes a tuple of 'set' and 'clear' words. Instead of
    accumulating the *desired value* of the register all through, it
    accumulates these set and clear words.
    The updateRegister function generates an Atom action to apply these set
    and clear words.

--- Language/Atom/MSP430/RegisterState.hs
+++ Language/Atom/MSP430/RegisterState.hs
@@ -1,19 +1,26 @@
 module Language.Atom.MSP430.RegisterState where
 
+import Language.Atom
 import Control.Monad.State
 import Data.Bits
 
 -- | Implements the State monad with a function to convert symbolic constants into
 --   the type required by the state.
-type RegisterState a b = State (b, a -> b)
+type RegisterState a b = State ((b, b), a -> b)
 
 set, clear :: (Bits b) => a -> RegisterState a b ()
--- | Set the bits of a RegisterState. In effect, s <- s || b.
-set   b = state $ \(v, c) -> ((), (v .|. c b, c))
--- | Clear the bits of a RegisterState. The effect is s <- s && ~b.
-clear b = state $ \(v, c) -> ((), (v .&. complement (c b), c))
+-- | Set bits of a RegisterState. In effect, s <- s || b.
+set   b = state $ \((sets, clears), conv) -> ((), ((sets .|. conv b, clears), conv))
+-- | Clear bits of a RegisterState. The effect is s <- s && ~b.
+clear b = state $ \((sets, clears), conv) -> ((), ((sets, clears .|. conv b), conv))
 
 -- | Run a register manipulation block on an empty register and return the resulting
 --   register state.
-execRegisterState :: (RegisterState a b ()) -> (b, a -> b) -> b
+execRegisterState :: (RegisterState a b ()) -> ((b, b), a -> b) -> (b, b)
 execRegisterState fn init = fst $ execState fn init
+
+-- | Update the state of a register given a set of commands.
+updateRegister :: (Assign b, IntegralE b, Bits b) => V b -> ((b, b), a -> b) -> RegisterState a b () -> Atom ()
+updateRegister reg init s = reg <== BWOr (BWAnd (value reg) (Const $ complement clears)) (Const sets)
+    where (sets, clears) = execRegisterState s init
+
