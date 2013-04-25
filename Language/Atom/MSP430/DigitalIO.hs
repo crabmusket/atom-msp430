2e0a653c087cde2d9e67d55ba471f95cae201707
Daniel Buckmaster <dan.buckmaster@gmail.com>
2013-04-25 12:24:29 +1000

    Now there are separate types for writing to DIR and OUT registers.

--- Language/Atom/MSP430/DigitalIO.hs
+++ Language/Atom/MSP430/DigitalIO.hs
@@ -1,6 +1,8 @@
 module Language.Atom.MSP430.DigitalIO (
-    IOOption (..),
+    IODir (..),
+    IOValue (..),
     IORegister,
+    IODirRegister,
     portDir, portDir',
     portOut, portOut',
     portIn'
@@ -11,39 +13,46 @@ import Language.Atom.MSP430.RegisterState
 import Data.Bits
 import Data.Word
 
--- | A type of RegisterState where you can set and clear IOOptions.
-type IORegister = RegisterState IOOption Word8
+-- | A type of RegisterState where you can set and clear IODirs.
+type IODirRegister = RegisterState IODir Word8
+
+-- | A type of RegisterState where you can set and clear IOValues.
+type IORegister = RegisterState IOValue Word8
 
 -- | These options configure the state of an IO direction register. These registers
 --   determine which pins act as inputs and which as outputs. Pins are input (0) by default;
 --   to reset input after you've set output, simply clear (Out x).
-data IOOption
-    = Out Int     -- ^ Puts a single pin in output mode.
-    | Bit Int     -- ^ Identical to Out.
-    | High Int    -- ^ Identical to Out.
-    | Outs [Int]  -- ^ Puts a list of pins in output mode.
-    | Bits [Int]  -- ^ Identical to Outs.
-    | Highs [Int] -- ^ Identical to Outs.
+data IODir
+    = Out Int    -- ^ Puts a single pin in output mode.
+    | Outs [Int] -- ^ Puts a list of pins in output mode.
 
--- | Convert an IOOption symbol to an actual word value.
-ioOptToWord8 :: IOOption -> Word8
-ioOptToWord8 o = case o of
-    Out x    -> bit x
-    Bit x    -> bit x
-    High x   -> bit x
-    Outs xs  -> sum $ map (bit) xs
-    Bits xs  -> sum $ map (bit) xs
-    Highs xs -> sum $ map (bit) xs
+-- | Convert an IODir symbol to an actual word value.
+ioDirOptToWord8 :: IODir -> Word8
+ioDirOptToWord8 o = case o of
+    Out x   -> bit x
+    Outs xs -> sum $ map bit xs
 
-portDir :: Int -> IORegister () -> Atom ()
-portDir p s = updateRegister (portDir' p) ((0, 0), ioOptToWord8) s
+data IOValue
+    = Pin Int    -- ^ Writes a single pin high.
+    | Pins [Int] -- ^ Writes a list of pins high.
+
+-- | Convert an IODir symbol to an actual word value.
+ioValToWord8 :: IOValue -> Word8
+ioValToWord8 o = case o of
+    Pin x   -> bit x
+    Pins xs -> sum $ map bit xs
+
+-- | Sets the direction of pins in an IO port.
+portDir :: Int -> IODirRegister () -> Atom ()
+portDir p s = updateRegister (portDir' p) ((0, 0), ioDirOptToWord8) s
 
 -- | Gets a reference to a numbered output port's direction control register.
 portDir' :: Int -> V Word8
 portDir' p = validatePort p $ "P" ++ show p ++ "DIR"
 
+-- | Sets the output values of the pins in an IO port.
 portOut :: Int -> IORegister () -> Atom ()
-portOut p s = updateRegister (portOut' p) ((0, 0), ioOptToWord8) s
+portOut p s = updateRegister (portOut' p) ((0, 0), ioValToWord8) s
 
 -- | Gets a reference to a numbered port's output register.
 portOut' :: Int -> V Word8
