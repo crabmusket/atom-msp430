8eb42cce0a2b0526288b3802c11a6bff34deba84
Daniel Buckmaster <dan.buckmaster@gmail.com>
2013-04-29 11:21:43 +1000

    Added REN register access.

--- Language/Atom/MSP430/DigitalIO.hs
+++ Language/Atom/MSP430/DigitalIO.hs
@@ -18,6 +18,9 @@ type IODirRegister = RegisterState IODir Word8
 -- | A type of RegisterState where you can set and clear IOValues.
 type IORegister = RegisterState IOValue Word8
 
+-- | RegisterState for the pullup/down register enable register.
+type IORENRegister = RegisterState IOResistor Word8
+
 -- | These options configure the state of an IO direction register. These registers
 --   determine which pins act as inputs and which as outputs. Pins are input (0) by default;
 --   to reset input after you've set output, simply clear (Out x).
@@ -41,6 +44,17 @@ ioValToWord8 o = case o of
     Pin x   -> bit x
     Pins xs -> sum $ map bit xs
 
+-- | Enable the pullup/down resistor of a pin.
+data IOResistor
+    = Resistor Int    -- ^ Enables a resistor on a single pin.
+    | Resistors [Int] -- ^ Enables resistors for a list of pins.
+
+-- | Converts an IOResistor symbol to a word.
+ioResToWord8 :: IOResistor -> Word8
+ioResToWord8 r = case r of
+    Resistor x   -> bit x
+    Resistors xs -> sum $ map bit xs
+
 -- | Sets the direction of pins in an IO port.
 portDir :: Int -> IODirRegister () -> Atom ()
 portDir p s = updateRegister (portDir' p) ((0, 0), ioDirOptToWord8) s
@@ -61,6 +75,14 @@ portOut' p = validatePort p $ "P" ++ show p ++ "OUT"
 portIn' :: Int -> E Word8
 portIn' p = value $ validatePort p $ "P" ++ show p ++ "IN"
 
+-- | Enables the pullup/down resistors attached to an IO port.
+portRen :: Int -> IORENRegister () -> Atom ()
+portRen p = updateRegister (portRen' p) ((0, 0), ioResToWord8)
+
+-- | Get a reference to a port's resistor enable register.
+portRen' :: Int -> V Word8
+portRen' p = validatePort p $ "P" ++ show p ++ "REN"
+
 -- | Validate a port number so we don't have people selecting the wrong ports.
 validatePort :: Int -> Name -> V Word8
 validatePort p = if p > 0 && p <= 8 then word8' else error $ "There is no port number " ++ show p
@@ -68,6 +90,8 @@ validatePort p = if p > 0 && p <= 8 then word8' else error $ "There is no port n
 port1Out = portOut 1
 port1Dir = portDir 1
 port1In = portIn' 1
+port1Ren = portRen 1
 port2Out = portOut 2
 port2Dir = portDir 2
 port2In = portIn' 2
+port2Ren = portRen 2
