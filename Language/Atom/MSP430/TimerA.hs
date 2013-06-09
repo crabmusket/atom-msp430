01ec24b0c4e14f61e7c9c60abd488c8cd06cba7c
Daniel Buckmaster <dan.buckmaster@gmail.com>
2013-06-09 21:31:42 +1000

    Added more Timer A vectors.

--- Language/Atom/MSP430/TimerA.hs
+++ Language/Atom/MSP430/TimerA.hs
@@ -3,9 +3,13 @@ module Language.Atom.MSP430.TimerA where
 import Language.Atom
 import Data.Word
 
+taSource           = 0x0000 :: Word16
+taSourceACLK       = 0x0100 :: Word16
+taSourceSMCLK      = 0x0200 :: Word16
+taSourceINCLK      = 0x0300 :: Word16
 taClear            = 0x0004 :: Word16
 taInterruptEnabled = 0x0002 :: Word16
-taStopMode         = 0x0    :: Word16
+taStopMode         = 0x0000 :: Word16
 taUpMode           = 0x0010 :: Word16
 taContinuousMode   = 0x0020 :: Word16
 taUpDownMode       = 0x0030 :: Word16
@@ -14,4 +18,9 @@ taSourceDiv4       = 0x0080 :: Word16
 taSourceDiv2       = 0x0040 :: Word16
 taSourceDiv1       = 0x0000 :: Word16
 
+taCCRInterrupt = 0x0010 :: Word16
+
 timerAControl = word16' "TACTL"
+timerACapComp0 = word16' "TACCR0"
+timerACapCompCtl0 = word16' "TACCTL0"
+timerAInterrupt = word16' "TAIV"
