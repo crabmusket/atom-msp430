64eb950c9d1bc288e60cbd2fa9bbe9e2590e8f63
Daniel Buckmaster <dan.buckmaster@gmail.com>
2013-06-10 16:37:21 +1000

    Added interrupt registers for DIO ports.

--- Language/Atom/MSP430/DigitalIO.hs
+++ Language/Atom/MSP430/DigitalIO.hs
@@ -1,7 +1,4 @@
-module Language.Atom.MSP430.DigitalIO (
-    port1In, port1Out, port1Dir, port1Resistors, port1Function,
-    port2In, port2Out, port2Dir, port2Resistors, port2Function
- ) where
+module Language.Atom.MSP430.DigitalIO where
 
 import Language.Atom
 
@@ -11,8 +8,17 @@ port1Dir        = word8' "P1DIR"
 port1Resistors  = word8' "P1REN"
 port1Function   = word8' "P1SEL"
 
+port1InterruptEnable = word8' "P1IE"
+port1InterruptFlags  = word8' "P1IFG"
+port1InterruptEdgeSelect = word8' "P1IES"
+
 port2In         = word8' "P2IN"
 port2Out        = word8' "P2OUT"
 port2Dir        = word8' "P2DIR"
 port2Resistors  = word8' "P2REN"
 port2Function   = word8' "P2SEL"
+
+port2InterruptEnable = word8' "P2IE"
+port2InterruptFlags  = word8' "P2IFG"
+port2InterruptEdgeSelect = word8' "P2IES"
+
