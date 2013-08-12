1dcf76afec725b08b44627bf949abe2ba0a4bde5
Daniel Buckmaster <dan.buckmaster@gmail.com>
2013-08-12 16:36:34 +1000

    Added inline Assembly.

--- Language/Atom/MSP430/Inline.hs
+++ Language/Atom/MSP430/Inline.hs
@@ -5,7 +5,14 @@ module Language.Atom.MSP430.Inline where
 import Language.Haskell.TH
 import Language.Haskell.TH.Quote
 import Language.Atom
+import Data.String.Utils
 
 c = QuasiQuoter {
     quoteExp = \code -> [| action (\_ -> code) [] |]
  }
+
+asm = QuasiQuoter {
+    quoteExp = \code -> [| action (\_ ->
+        unlines $ map (\l -> "__asm__(\"" ++ (strip l) ++ "\");") $ lines $ code) [] |]
+ }
+
